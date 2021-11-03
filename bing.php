a4c7f4a19c234c5dfa4cf85b8e3dbc7899b9f8e8
hzz <dexter.ho.cn@gmail.com>
2021-11-03 15:16:56 +0800

    fix: replace download url

--- bing.php
+++ bing.php
@@ -1,15 +1,20 @@
 <?php
+const BING_HOST = "https://www.bing.com";
+const SAVE_PATH = "F:/bin_pic/";
 
-$content = file_get_contents('https://area.sinaapp.com/bingImg');
+$content = file_get_contents(BING_HOST.'/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US');
+$pic_url = json_decode($content,true)["images"][0]["url"] ?? "";
 
-// 路径
-$path = "C:/E/pic/wallpaper/";
-
-$file = file_put_contents($path.date('Y-m-d').'.jpg', $content);
-
-if (is_int($file) && $file > 0 )
+if ( !empty($pic_url) )
 {
-	echo "下载成功";
+    $picContent = file_get_contents(BING_HOST.$pic_url);
+    $file = file_put_contents(SAVE_PATH.date('Y-m-d').'.jpg', $picContent);
+    if (is_int($file) && $file > 0 )
+    {
+        var_dump( "下载成功",$file);
+    }else{
+        var_dump( "下载失败",$file);
+    }
 }else{
-	echo $file;
+    echo "下载失败，bing 地址获取失败";
 }
--- readme.md
+++ readme.md
@@ -3,7 +3,7 @@
 ## 使用
 - > git clone git@github.com:hezhizheng/bing.git
 - > cd bing/
-- > vim bing.php // 替换壁纸的保存路径，修改 $path
+- > vim bing.php // 替换壁纸的保存路径，修改 常量 SAVE_PATH
 - > php bing.php // 执行下载
 
 &emsp;&emsp;![微信截图_20190902140227.png](https://i.loli.net/2019/09/02/WrAvwVK6scbmEy2.png)
@@ -12,4 +12,4 @@
 > 1 0 * * * * php bing.php  // 每天 00:01:00 自动执行下载
 
 ## Windows
-- 自行参考Windows的计划任务设置
+- 推荐 Windows 定时任务管理器 [go-crontab](https://github.com/hezhizheng/go-crontab)
